dc3887f9857d3366f7b70cea5a519fa83e601838
Raymond Loman <r.loman@recognize.nl>
2023-07-17 15:46:05 +0200

    Add objects

---
+++ objects/main.js
@@ -0,0 +1,32 @@
+function printMe(person) {
+
+
+    for (let property in person) {
+        console.log(`Person has property: ${property}`)
+        console.log(`Person has property: ${property} and the value is: ${person[property]}`)
+    }
+    console.log(`Person lastName is: ${person.lastName}`)
+    // via de associatieve array
+    console.log(person["firstName"])
+
+
+    // ?.
+    console.log(person.partner)
+    console.log(person?.partner?.length)
+
+
+    // ??
+    person.partner = person.partner ?? "Nieuwe partner"
+    console.log(person.partner)
+}
+
+let person = new Object
+person.firstName = 'John'
+person.lastName = 'Smith'
+person.dateOfBirth= '1968-08-09'
+person.hobbies = ["Hardlopen", "Lezen", "Programming"]
+// person.partner="Suzanne"
+printMe(person)
+
+
+
